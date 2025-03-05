5bab5506cf4ff3ee55c5692dd74d458a3eb244ec
zlatin-r <zlatinrusev@gmail.com>
2025-03-05 19:28:48 +0100

    added path login/

--- fitlog/accounts/urls.py
+++ fitlog/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from fitlog.accounts.views import RegisterView
+from fitlog.accounts.views import RegisterView, LogingView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register')
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LogingView.as_view(), name='login'),
 ]
