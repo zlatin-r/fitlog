d5ced9e51e9c534896bb63feea16b137a4deb523
zlatin-r <zlatinrusev@gmail.com>
2025-03-05 18:49:26 +0100

    added RegisterView

--- fitlog/accounts/views.py
+++ fitlog/accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from fitlog.accounts.serializers import UserSerializer
+
+UserModel = get_user_model()
+
+class RegisterView(CreateAPIView):
+    queryset = UserModel
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
