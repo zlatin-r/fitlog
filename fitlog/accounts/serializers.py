6a720745285400ee669ecafca127236900717da6
zlatin-r <zlatinrusev@gmail.com>
2025-03-05 20:33:18 +0100

    added LogoutRequestSerializer and LogoutResponseSerializer serializers

--- fitlog/accounts/serializers.py
+++ fitlog/accounts/serializers.py
@@ -26,3 +26,11 @@ class LoginResponseSerializer(serializers.Serializer):
     refresh = serializers.CharField()
     access = serializers.CharField()
     message = serializers.CharField()
+
+
+class LogoutRequestSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+
+
+class LogoutResponseSerializer(serializers.Serializer):
+    message = serializers.CharField()
